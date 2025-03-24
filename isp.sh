5110834e0cc9a0f6a605ee7a7cb49b84326ad3a7
sysdata <ebrou833@gmail.com>
2025-03-24 22:30:06 +0000

    Update isp.sh

--- isp.sh
+++ isp.sh
@@ -18,11 +18,9 @@ table inet nat {
 
  sudo systemctl enable --now nftables > /dev/null 2>&1
 
-history -s "nano /etc/sysctl.conf"
-history -s "sysctl -p"
-history -s "nano /etc/nftables/isp.nft"
-history -s "nano /etc/sysconfig/nftables.conf"
-history -s "systemctl enable --now nftables"
-
-
- history -w
+ history -s "nano /etc/sysctl.conf"
+ history -s "sysctl -p"
+ history -s "nano /etc/nftables/isp.nft"
+ history -s "nano /etc/sysconfig/nftables.conf"
+ history -s "systemctl enable --now nftables"
+ history -a
