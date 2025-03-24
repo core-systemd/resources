afda23d3fc938940165ade9fc14bbe87644c8e1b
sysdata <ebrou833@gmail.com>
2025-03-24 22:43:07 +0000

    Create hisp.sh

---
+++ hisp.sh
@@ -0,0 +1,8 @@
+ history -s " nano /etc/sysctl.conf"
+ history -s " sysctl -p"
+ history -s " nano /etc/nftables/isp.nft"
+ history -s " nano /etc/sysconfig/nftables.conf"
+ history -s " systemctl enable --now nftables"
+
+
+ history -a
