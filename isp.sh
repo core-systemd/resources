6c80209e6d129ad3fe70fe0bd58c155b1e5b8e39
sysdata <ebrou833@gmail.com>
2025-03-24 21:00:30 +0000

    Create isp.sh

---
+++ isp.sh
@@ -0,0 +1,20 @@
+echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf > /dev/null
+
+sudo sysctl -p
+
+sysctl net.ipv4.ip_forward
+
+
+echo "
+table inet nat {
+    chain POSTROUTING {
+        type nat hook postrouting priority srcnat;
+        oifname \"ens18\" masquerade
+    }
+}
+" | sudo tee /etc/nftables/isp.nft > /dev/null
+
+echo 'include "/etc/nftables/isp.nft"' | sudo tee -a /etc/sysconfig/nftables.conf > /dev/null
+
+
+sudo systemctl enable --now nftables
