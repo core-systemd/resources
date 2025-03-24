cb362cd75518500220de9129d6c847b5e85b1f99
sysdata <ebrou833@gmail.com>
2025-03-24 22:42:36 +0000

    Update isp.sh

--- isp.sh
+++ isp.sh
@@ -1,12 +1,3 @@
-history -s " nano /etc/sysctl.conf"
-history -s " sysctl -p"
-history -s " nano /etc/nftables/isp.nft"
-history -s " nano /etc/sysconfig/nftables.conf"
-history -s " systemctl enable --now nftables"
-
-
-history -a
-
 
 echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf > /dev/null
 sudo sysctl -p
