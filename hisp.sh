81ff520f6629048b06e7e6f34b04396b1fc32361
sysdata <ebrou833@gmail.com>
2025-03-25 00:39:46 +0000

    Update hisp.sh

--- hisp.sh
+++ hisp.sh
@@ -1,8 +1,33 @@
- history -s " nano /etc/sysctl.conf"
- history -s " sysctl -p"
- history -s " nano /etc/nftables/isp.nft"
- history -s " nano /etc/sysconfig/nftables.conf"
- history -s " systemctl enable --now nftables"
+# Устанавливаем nftables (если не установлен)
+echo "Установка nftables..."
+dnf install -y nftables
 
+# Создаем файл конфигурации для ISP
+echo "Создание файла конфигурации /etc/nftables/isp.nft..."
+cat <<EOT > /etc/nftables/isp.nft
+table inet nat {
+    chain POSTROUTING {
+        type nat hook postrouting priority srcnat;
+        oifname "ens18" masquerade
+    }
+}
+EOT
 
- history -a
+# Добавляем include в конфигурацию nftables, если его там нет
+NFT_CONFIG="/etc/sysconfig/nftables.conf"
+INCLUDE_STRING='include "/etc/nftables/isp.nft"'
+
+if ! grep -qF "$INCLUDE_STRING" "$NFT_CONFIG"; then
+    echo "Добавление конфигурации ISP в /etc/sysconfig/nftables.conf..."
+    echo "$INCLUDE_STRING" >> "$NFT_CONFIG"
+fi
+
+# Включаем и запускаем nftables
+echo "Запуск и добавление в автозагрузку nftables..."
+systemctl enable --now nftables
+
+# Проверка конфигурации
+echo "Проверка загруженной конфигурации nftables..."
+nft list ruleset
+
+echo "Настройка NAT на ISP завершена!"
